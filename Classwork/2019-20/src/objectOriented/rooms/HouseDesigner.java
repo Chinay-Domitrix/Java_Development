db28084e2c2d67da0e37f522ac309d8490d6f3bf
Chinay1023 <55807470+Chinay1023@users.noreply.github.com>
2019-12-04 18:35:23 -0500

    added all work

--- Classwork/2019-20/src/objectOriented/rooms/HouseDesigner.java
+++ Classwork/2019-20/src/objectOriented/rooms/HouseDesigner.java
@@ -2,7 +2,10 @@ package objectOriented.rooms;
 
 public class HouseDesigner {
 	private HouseDesigner() {
-
+		System.out.println("Java House Layout");
+		for (var i = 0; i < 20; i++) {
+			System.out.println('\u2015');
+		}
 	}
 
 	public static void main(String[] args) {
