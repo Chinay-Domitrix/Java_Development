85cd03d56a7793780d9a7457d0e88855c3d42540
Chinay-Domitrix <baviskarchirag@gmail.com>
2021-10-29 09:06:55 -0400

    Update project
    Signed-off-by: Chinay-Domitrix <baviskarchirag@gmail.com>

--- Classwork/dataStructures/src/maze/Explorer.kt
+++ Classwork/dataStructures/src/maze/Explorer.kt
@@ -1,25 +1,72 @@
 package maze
 
+import java.awt.Color
+import java.awt.Rectangle
+
+
+class Explorer(var color: Color, var location: Location, var size: Int, var dir: Int) {
+	fun move(keyCode: Int, maze: Array<Array<String?>>, draw3D: Boolean): Int {
+		if (draw3D == false) {
+			if (keyCode == 87 && maze[location.y - 1][location.x] != "#") location.y =
+				location.y - 1 else if (keyCode == 65 && maze[location.y][location.x - 1] != "#") {
+				location.x = location.x - 1
+				dir--
+				if (dir == -4) dir = 0
+			} else if (keyCode == 83 && maze[location.y + 1][location.x] != "#") location.y =
+				(location.y + 1) else if (keyCode == 68 && maze[location.y][location.x + 1] != "#") {
+				location.x = (location.x + 1)
+				dir++
+				if (dir == 4) dir = 0
+			}
+		} else if (keyCode == 87) when (dir) {
+			0 -> location.x = location.x + 1
+			1 -> location.y = location.y + 1
+			2 -> location.x = location.x - 1
+			3 -> location.y = location.y - 1
+			-1 -> location.y = location.y - 1
+			-2 -> location.x = location.x - 1
+			-3 -> location.y = location.y + 1
+		} else if (keyCode == 65) {
+			dir--
+			if (dir == -4) dir = 0
+		} else if (keyCode == 68) {
+			dir++
+			if (dir == 4) dir = 0
+		}
+		return dir
+	}
+
+	val rectangle: Rectangle get() = Rectangle(location.x * size, location.y * size, size, size)
+}
+/*
 import maze.Explorer.Direction.*
 import java.awt.Color
 import java.awt.Rectangle
 
 class Explorer(
-	/**
-	 * The `Explorer`'s location
-	 */
+	*/
+/**
+ * The `Explorer`'s location
+ *//*
+
 	var location: Location,
-	/**
-	 * The `Explorer`'s direction, represented by an `int`
-	 */
+	*/
+/**
+ * The `Explorer`'s direction, represented by an `int`
+ *//*
+
 	var direction: Direction,
-	/**
-	 * The `Explorer`'s size
-	 */
+	*/
+/**
+ * The `Explorer`'s size
+ *//*
+
 	val size: Int,
-	/**
-	 * The `Explorer`'s color
-	 */
+	*/
+/**
+ * The `Explorer`'s color
+ *//*
+
 	val color: Color
 ) {
 	constructor(location: Location, direction: Int, size: Int, color: Color) : this(
@@ -51,3 +98,4 @@ class Explorer(
 		move((if (i == 0) NORTH else if (i == 1) WEST else if (i == 2) EAST else if (i == 3) SOUTH else null) as Direction)
 	}
 }
+*/
--- Classwork/dataStructures/src/maze/Maze.kt
+++ Classwork/dataStructures/src/maze/Maze.kt
@@ -4,6 +4,256 @@ import java.awt.Color.*
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+import java.awt.event.MouseEvent
+import java.awt.event.MouseListener
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.FileReader
+import javax.swing.JFrame
+import javax.swing.JPanel
+
+class Maze : JPanel(), KeyListener, MouseListener {
+	var frame: JFrame
+	lateinit var maze: Array<Array<String?>>
+	var explorer: Explorer? = null
+	var isPainted = false
+	var alreadyRead = false
+	var lineArr = arrayOfNulls<String>(33)
+	var k = 0
+	var line: String? = null
+	var lineCount = 0
+	var draw3D = false
+	var shrink = 50
+	lateinit var walls: ArrayList<Wall>
+	public override fun paintComponent(g: Graphics) {
+		super.paintComponent(g)
+		val g2 = g as Graphics2D
+		g.setColor(BLACK)
+		g.fillRect(0, 0, 1600, 1000)
+		k = 0
+		if (draw3D == false) {
+			maze = Array(34) { arrayOfNulls<String>(34) }
+//			maze = new String[lineArr.length][lineArr[0].split("").length];
+			for (i in lineArr.indices) lineArr[0]?.split("".toRegex())?.toTypedArray()?.indices?.forEach {
+				maze[i][it] = lineArr[i]?.split("".toRegex())?.toTypedArray()?.get(it)
+				if (maze[i][it] == "#") g.setColor(WHITE) else if (maze[i][it] == "1" && isPainted == false) {
+					explorer = Explorer(RED, Location(it, i), 15, 0)
+					g.setColor(explorer?.color)
+					isPainted = true
+				} else if (maze[i][it] == "E") g.setColor(GREEN) else g.setColor(BLACK)
+				g.fillRect(it * 15, i * 15, 15, 15)
+			}
+			if (isPainted == true) {
+				g.setColor(explorer?.color)
+				g2.fill(explorer?.rectangle)
+			}
+		} else if (draw3D) walls.indices.forEach {
+			g2.color = WHITE
+			g2.fillPolygon(walls[it].polygon)
+			g2.color = RED
+			g2.drawPolygon(walls[it].polygon)
+		}
+	}
+
+	fun createWalls() {
+		walls = ArrayList()
+		for (n in 0..4) {
+			walls.add(getCeil(n))
+			walls.add(getFloor(n))
+			//walls.add(getLeft(n));
+			//walls.add(getRight(n));
+		}
+		val explorerX = explorer?.location?.x
+		val explorerY = explorer?.location?.y
+		val explorerDirection = explorer?.dir
+		when (explorerDirection) {
+			0 -> {
+				var n = 0
+				while (n < 5) {
+					try {
+						//if(maze[expR-1][expC-(5-n)] == "#"){
+						//	walls.add(getFront(5-n));
+						//}
+						if (maze[explorerY!! + 1][explorerX!! + n] == "#") walls.add(getRight(n))
+						if (maze[explorerY - 1][explorerX + n] == "#") walls.add(getLeft(n))
+					} catch (e: ArrayIndexOutOfBoundsException) {
+						e.printStackTrace()
+					}
+					n++
+				}
+			}
+			1 -> {
+				var n = 0
+				while (n < 5) {
+					try {
+						//if(maze[expR-1][expC-(5-n)] == "#"){
+						//	walls.add(getFront(5-n));
+						//}
+						if (maze[explorerY!! + n][explorerX!! - 1] == "#") walls.add(getRight(n))
+						if (maze[explorerY + n][explorerX + 1] == "#") walls.add(getLeft(n))
+					} catch (e: ArrayIndexOutOfBoundsException) {
+						e.printStackTrace()
+					}
+					n++
+				}
+			}
+			2 -> {
+				var n = 0
+				while (n < 5) {
+					try {
+						//if(maze[expR-1][expC-(5-n)] == "#"){
+						//	walls.add(getFront(5-n));
+						//}
+						if (maze[explorerY!! + 1][explorerX!! - n] == "#") walls.add(getRight(n))
+						if (maze[explorerY - 1][explorerX - n] == "#") walls.add(getLeft(n))
+					} catch (e: ArrayIndexOutOfBoundsException) {
+						e.printStackTrace()
+					}
+					n++
+				}
+			}
+			3 -> {
+				var n = 0
+				while (n < 5) {
+					try {
+						//if(maze[expR-1][expC-(5-n)] == "#"){
+						//	walls.add(getFront(5-n));
+						//}
+						if (maze[explorerY!! - n][explorerX!! + 1] == "#") walls.add(getRight(n))
+						if (maze[explorerY - n][explorerX - 1] == "#") walls.add(getLeft(n))
+					} catch (e: ArrayIndexOutOfBoundsException) {
+						e.printStackTrace()
+					}
+					n++
+				}
+			}
+			-1 -> {
+				var n = 0
+				while (n < 5) {
+					try {
+						//if(maze[expR-1][expC-(5-n)] == "#"){
+						//	walls.add(getFront(5-n));
+						//}
+						if (maze[explorerY!! - n][explorerX!! + 1] == "#") walls.add(getRight(n))
+						if (maze[explorerY - n][explorerX - 1] == "#") walls.add(getLeft(n))
+					} catch (e: ArrayIndexOutOfBoundsException) {
+						e.printStackTrace()
+					}
+					n++
+				}
+			}
+			-2 -> {
+				var n = 0
+				while (n < 5) {
+					try {
+						//if(maze[expR-1][expC-(5-n)] == "#"){
+						//	walls.add(getFront(5-n));
+						//}
+						if (maze[explorerY!! + 1][explorerX!! - n] == "#") walls.add(getRight(n))
+						if (maze[explorerY - 1][explorerX - n] == "#") walls.add(getLeft(n))
+					} catch (e: ArrayIndexOutOfBoundsException) {
+						e.printStackTrace()
+					}
+					n++
+				}
+			}
+			-3 -> {
+				var n = 0
+				while (n < 5) {
+					try {
+						//if(maze[expR-1][expC-(5-n)] == "#"){
+						//	walls.add(getFront(5-n));
+						//}
+						if (maze[explorerY!! + n][explorerX!! - 1] == "#") walls.add(getRight(n))
+						if (maze[explorerY + n][explorerX + 1] == "#") walls.add(getLeft(n))
+					} catch (e: ArrayIndexOutOfBoundsException) {
+						e.printStackTrace()
+					}
+					n++
+				}
+			}
+		}
+	}
+
+	fun getFloor(n: Int) = Wall(
+		intArrayOf(700 - n * shrink, 650 - n * shrink, 650 - n * shrink, 700 - n * shrink),
+		intArrayOf(100 + n * shrink, 150 + n * shrink, 650 - n * shrink, 700 - n * shrink),
+		"Floor"
+	)
+
+
+	fun getCeil(n: Int) = Wall(
+		intArrayOf(100 + n * shrink, 150 + n * shrink, 150 + n * shrink, 100 + n * shrink),
+		intArrayOf(100 + n * shrink, 150 + n * shrink, 650 - n * shrink, 700 - n * shrink),
+		"Ceil"
+	)
+
+	fun getLeft(n: Int) = Wall(
+		intArrayOf(100 + n * shrink, 150 + n * shrink, 650 - n * shrink, 700 - n * shrink),
+		intArrayOf(100 + n * shrink, 150 + n * shrink, 150 + n * shrink, 100 + n * shrink),
+		"Left"
+	)
+
+	fun getRight(n: Int) = Wall(
+		intArrayOf(100 + n * shrink, 150 + n * shrink, 650 - n * shrink, 700 - n * shrink),
+		intArrayOf(700 - n * shrink, 650 - n * shrink, 650 - n * shrink, 700 - n * shrink),
+		"Right"
+	)
+
+	fun getFront(n: Int) = Wall(
+		intArrayOf(100 + n * shrink, 100 + n * shrink, 700 - n * shrink, 700 - n * shrink),
+		intArrayOf(100 + n * shrink, 650 - n * shrink, 650 - n * shrink, 700 - n * shrink),
+		"Ceil"
+	)
+
+	override fun keyPressed(e: KeyEvent) {
+		if (e.keyCode == 32) draw3D = !draw3D
+		if (draw3D) createWalls()
+		explorer?.move(e.keyCode, maze, draw3D)
+		repaint()
+	}
+
+	override fun keyReleased(e: KeyEvent) {}
+	override fun keyTyped(e: KeyEvent) {}
+	override fun mouseClicked(e: MouseEvent) {}
+	override fun mousePressed(e: MouseEvent) {}
+	override fun mouseReleased(e: MouseEvent) {}
+	override fun mouseEntered(e: MouseEvent) {}
+	override fun mouseExited(e: MouseEvent) {}
+
+	companion object {
+		@JvmStatic
+		fun main(args: Array<String>) {
+			Maze()
+		}
+	}
+
+	init {
+		frame = JFrame()
+		frame.add(this)
+		frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+		frame.setSize(1600, 1000)
+		frame.isVisible = true
+		frame.addKeyListener(this)
+		frame.addMouseListener(this)
+		try {
+			val reader = BufferedReader(FileReader("Classwork/dataStructures/src/maze/Maze.txt"))
+			while (reader.readLine().also { line = it } != null) {
+				lineArr[k] = line
+				lineCount++
+				k++
+			}
+		} catch (e: FileNotFoundException) {
+			e.printStackTrace()
+		}
+	}
+}
+/*
+import java.awt.Color.*
+import java.awt.Graphics
+import java.awt.Graphics2D
+import java.awt.event.KeyEvent
 import java.awt.event.KeyEvent.*
 import java.awt.event.KeyListener
 import java.awt.event.MouseEvent
@@ -47,7 +297,9 @@ class Maze : JPanel(), KeyListener, MouseListener {
 		repaint()
 		if (!draw3D) {
 			for (i in list.indices) (0 until list[i].length).forEach { j ->
-				with(list[i].split("")/*.toTypedArray()*/) {
+				with(list[i].split("")*/
+/*.toTypedArray()*//*
+) {
 					if (this[j] == "#") {
 						g.setColor(black)
 						g.fillRect(j * 13, i * 13, 13, 13)
@@ -151,4 +403,4 @@ class Maze : JPanel(), KeyListener, MouseListener {
 			addMouseListener(this@Maze)
 		}
 	}
-}
+}*/
