14dd7965ba7ff2d41fd9da20c66ca5eb2b009baf
Chinay-Domitrix <baviskarchirag@gmail.com>
2021-11-09 08:53:47 -0500

    Update project
    Signed-off-by: Chinay-Domitrix <baviskarchirag@gmail.com>

--- Classwork/dataStructures/src/maze2/Explorer.kt
+++ Classwork/dataStructures/src/maze2/Explorer.kt
@@ -7,18 +7,30 @@ class Explorer(var color: Color, var location: Location, var size: Int, var dir:
 	fun move(keyCode: Int, maze: Array<Array<String>>, draw3D: Boolean): Int {
 		//87 = W, 83 = S, 65 = A, 68 = D
 		if (draw3D == false) {
-			if (keyCode == 87 && maze[location.y - 1][location.x] != "#") {
+			if (keyCode == 87 && !maze[location.y - 1][location.x].equals("#")) {
 				location.y = location.y - 1
-			} else if (keyCode == 65 && maze[location.y][location.x - 1] != "#") {
+			} else if (keyCode == 65 && !maze[location.y][location.x - 1].equals("#")) {
 				location.x = location.x - 1
-			} else if (keyCode == 83 && maze[location.y + 1][location.x] != "#") {
+			} else if (keyCode == 83 && !maze[location.y + 1][location.x].equals("#")) {
 				location.y = location.y + 1
-			} else if (keyCode == 68 && maze[location.y][location.x + 1] != "#") {
+			} else if (keyCode == 68 && !maze[location.y][location.x + 1].equals("#")) {
 				location.x = location.x + 1
 			}
 		} else {
 			if (keyCode == 87) {
-				when (dir) {
+				try {
+					location.run {
+						when (dir) {
+							0 -> if (!maze[y][x].equals("#")) x = x + 1
+							1, -3 -> if (!maze[y + 1][x].equals("#")) y = y + 1
+							2, -2 -> if (!maze[y][x - 1].equals("#")) x = x - 1
+							3, -1 -> if (!maze[y - 1][x].equals("#")) y = y - 1
+						}
+					}
+				} catch (e: ArrayIndexOutOfBoundsException) {
+					e.printStackTrace()
+				}
+				/*when (dir) {
 					0 -> location.x = location.x + 1
 					1 -> location.y = location.y + 1
 					2 -> location.x = location.x - 1
@@ -26,7 +38,7 @@ class Explorer(var color: Color, var location: Location, var size: Int, var dir:
 					-1 -> location.y = location.y - 1
 					-2 -> location.x = location.x - 1
 					-3 -> location.y = location.y + 1
-				}
+				}*/
 			} else if (keyCode == 65) {
 				dir--
 				if (dir == -4) {
@@ -44,4 +56,5 @@ class Explorer(var color: Color, var location: Location, var size: Int, var dir:
 
 	val rectangle: Rectangle
 		get() = Rectangle(location.x * size, location.y * size, size, size)
+
 }
